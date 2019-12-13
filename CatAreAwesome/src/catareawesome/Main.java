d5fdd892100f951fc4673dd8f2ee259e4803333f
lykoh2000 <36021624+lykoh2000@users.noreply.github.com>
2019-12-13 08:06:02 +0800

    Update and rename test.java to Main.java

--- CatAreAwesome/src/catareawesome/Main.java
+++ CatAreAwesome/src/catareawesome/Main.java
@@ -11,7 +11,7 @@ import static javafx.application.Application.launch;
 import javafx.stage.Stage;
 import static catareawesome.firstScene.window;
 
-public class test extends Application{
+public class Main extends Application{
 
     public static void main(String[] args) {
         launch(args);
